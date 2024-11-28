702d0973388271dfb8e59f5a873106945a977114
Aykadra <92817177+Aykadra@users.noreply.github.com>
2024-11-28 10:22:33 +0000

    step load mesh et job

--- abaqus.py
+++ abaqus.py
@@ -101,3 +101,90 @@ mdb.models['Model-3D-poutre'].rootAssembly.translate(instanceList=('Part-3-1',
     ), vector=(50.0, 50.0, 0.0))
 mdb.models['Model-3D-poutre'].rootAssembly.translate(instanceList=('Part-2-1', 
     ), vector=(150.0, 100.0, 0.0))
+
+## Step
+mdb.models['Model-3D-poutre'].StaticStep(description=
+    'Calcul des contraintes résultantes de la pression interne de 150 bar', 
+    name='Etude Statique', previous='Initial')
+
+## Load
+# mdb.models['Model-3D-poutre'].rootAssembly.Surface(end2Edges=
+#     mdb.models['Model-3D-poutre'].rootAssembly.instances['Tube_vertical-1'].edges.getSequenceFromMask(
+#     mask=('[#1 ]', ), )+\
+#     mdb.models['Model-3D-poutre'].rootAssembly.instances['Part-3-1'].edges.getSequenceFromMask(
+#     mask=('[#1 ]', ), )+\
+#     mdb.models['Model-3D-poutre'].rootAssembly.instances['Part-2-1'].edges.getSequenceFromMask(
+#     mask=('[#1 ]', ), ), name='surface1')
+# mdb.models['Model-3D-poutre'].Pressure(amplitude=UNSET, createStepName=
+#     'Etude Statique', distributionType=UNIFORM, field='', magnitude=15.0, name=
+#     'Chargement pression', region=
+#     mdb.models['Model-3D-poutre'].rootAssembly.surfaces['surface1'])
+
+mdb.models['Model-3D-poutre'].rootAssembly.Set(edges=
+    mdb.models['Model-3D-poutre'].rootAssembly.instances['Tube_vertical-1'].edges.getSequenceFromMask(
+    mask=('[#1 ]', ), )+\
+    mdb.models['Model-3D-poutre'].rootAssembly.instances['Part-3-1'].edges.getSequenceFromMask(
+    mask=('[#1 ]', ), )+\
+    mdb.models['Model-3D-poutre'].rootAssembly.instances['Part-2-1'].edges.getSequenceFromMask(
+    mask=('[#1 ]', ), ), name='tuyau complet')
+mdb.models['Model-3D-poutre'].PipePressure(amplitude=UNSET, createStepName=
+    'Etude Statique', diameter=40.0, distributionType=UNIFORM, field='', 
+    magnitude=15.0, name='Load-2', region=
+    mdb.models['Model-3D-poutre'].rootAssembly.sets['tuyau complet'], side=
+    INTERNAL)
+
+## Mesh
+mdb.models['Model-3D-poutre'].parts['Part-2'].seedPart(deviationFactor=0.1, 
+    minSizeFactor=0.1, size=10.0)
+mdb.models['Model-3D-poutre'].parts['Part-2'].setElementType(elemTypes=(
+    ElemType(elemCode=B31, elemLibrary=STANDARD), ), regions=(
+    mdb.models['Model-3D-poutre'].parts['Part-2'].edges.getSequenceFromMask((
+    '[#1 ]', ), ), ))
+mdb.models['Model-3D-poutre'].parts['Part-2'].generateMesh()
+
+mdb.models['Model-3D-poutre'].parts['Part-3'].seedPart(deviationFactor=0.1, 
+    minSizeFactor=0.1, size=3.0)
+mdb.models['Model-3D-poutre'].parts['Part-3'].setElementType(elemTypes=(
+    ElemType(elemCode=B31, elemLibrary=STANDARD), ), regions=(
+    mdb.models['Model-3D-poutre'].parts['Part-3'].edges.getSequenceFromMask((
+    '[#1 ]', ), ), ))
+mdb.models['Model-3D-poutre'].parts['Part-3'].generateMesh()
+
+mdb.models['Model-3D-poutre'].parts['Tube_vertical'].seedPart(deviationFactor=
+    0.1, minSizeFactor=0.1, size=10.0)
+mdb.models['Model-3D-poutre'].parts['Tube_vertical'].setElementType(elemTypes=(
+    ElemType(elemCode=B31, elemLibrary=STANDARD), ), regions=(
+    mdb.models['Model-3D-poutre'].parts['Tube_vertical'].edges.getSequenceFromMask(
+    ('[#1 ]', ), ), ))
+mdb.models['Model-3D-poutre'].parts['Tube_vertical'].generateMesh()
+
+## Beam orientation
+mdb.models['Model-3D-poutre'].parts['Tube_vertical'].Set(edges=
+    mdb.models['Model-3D-poutre'].parts['Tube_vertical'].edges.getSequenceFromMask(
+    ('[#1 ]', ), ), name='tige verticale')
+mdb.models['Model-3D-poutre'].parts['Tube_vertical'].assignBeamSectionOrientation(
+    method=N1_COSINES, n1=(0.0, 0.0, -1.0), region=
+    mdb.models['Model-3D-poutre'].parts['Tube_vertical'].sets['tige verticale'])
+mdb.models['Model-3D-poutre'].parts['Part-3'].Set(edges=
+    mdb.models['Model-3D-poutre'].parts['Part-3'].edges.getSequenceFromMask((
+    '[#1 ]', ), ), name='Set-3')
+mdb.models['Model-3D-poutre'].parts['Part-3'].assignBeamSectionOrientation(
+    method=N1_COSINES, n1=(0.0, 0.0, -1.0), region=
+    mdb.models['Model-3D-poutre'].parts['Part-3'].sets['Set-3'])
+mdb.models['Model-3D-poutre'].parts['Part-2'].Set(edges=
+    mdb.models['Model-3D-poutre'].parts['Part-2'].edges.getSequenceFromMask((
+    '[#1 ]', ), ), name='Set-4')
+mdb.models['Model-3D-poutre'].parts['Part-2'].assignBeamSectionOrientation(
+    method=N1_COSINES, n1=(0.0, 0.0, -1.0), region=
+    mdb.models['Model-3D-poutre'].parts['Part-2'].sets['Set-4'])
+
+## Job
+mdb.models['Model-3D-poutre'].rootAssembly.regenerate()
+mdb.Job(atTime=None, contactPrint=OFF, description='', echoPrint=OFF, 
+    explicitPrecision=SINGLE, getMemoryFromAnalysis=True, historyPrint=OFF, 
+    memory=90, memoryUnits=PERCENTAGE, model='Model-3D-poutre', modelPrint=OFF, 
+    multiprocessingMode=DEFAULT, name='First_try', nodalOutputPrecision=SINGLE, 
+    numCpus=1, numGPUs=0, numThreadsPerMpiProcess=1, queue=None, resultsFormat=
+    ODB, scratch='', type=ANALYSIS, userSubroutine='', waitHours=0, 
+    waitMinutes=0)
+mdb.jobs['First_try'].submit(consistencyChecking=OFF)
